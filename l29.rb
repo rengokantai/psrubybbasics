bc03c676aaabf7948dc08c6405c4cb734f96818f
Hernan Y.Ke <yuriqiao@gmail.com>
2015-09-22 18:46:24 -0400

    2-9

---
+++ l29.rb
@@ -0,0 +1,17 @@
+
+
+# In ruby, we need to use extend keyword to 'extend' all prop of a module
+module Test
+  attr_accessor :start
+
+end
+
+class L29
+  include Test
+end
+
+
+usera = L29.new.extend(Test)  # singleton
+userb= L29.new
+puts usera.methods.grep(/start/)
+puts userb.methods.grep(/start/)
