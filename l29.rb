bde62034775be9f2e96813061834c6424a4f01e7
Hernan Y.Ke <yuriqiao@gmail.com>
2015-09-23 18:51:53 -0400

    Add singleton_class

--- l29.rb
+++ l29.rb
@@ -7,11 +7,13 @@ module Test
 end
 
 class L29
-  include Test
+  #include Test
 end
 
 
 usera = L29.new.extend(Test)  # singleton
 userb= L29.new
 puts usera.methods.grep(/start/)
-puts userb.methods.grep(/start/)
+puts usera.singleton_class.public_instance_methods.grep(/start/)
+puts userb.methods.grep(/start/) #nothing
+puts userb.singleton_class.public_instance_methods.grep(/start/) #nothing
